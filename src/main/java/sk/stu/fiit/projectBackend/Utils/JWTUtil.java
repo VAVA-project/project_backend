85351075fe8eef74d5064058cadd176a29b51b48
Adam Bublavy <bublavy.adam@gmail.com>
2021-03-28 14:34:45 +0200

    Added: JWTUtil

---
+++ src/main/java/sk/stu/fiit/projectBackend/Utils/JWTUtil.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.stu.fiit.projectBackend.Utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Adam Bublavý
+ */
+@Component
+public class JWTUtil implements Serializable {
+
+    private static final String SECRET_KEY = "secret";
+
+    private static final long JWT_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60;
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).
+                getBody();
+    }
+
+    public String extractUsernameFromToken(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpirationDateFromToken(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpirationDateFromToken(token).before(new Date());
+    }
+
+    public String generateToken(String token) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(token, claims);
+    }
+
+    private String createToken(String subject, Map<String, Object> claims) {
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(
+                new Date(System.currentTimeMillis())).setExpiration(new Date(
+                        System.currentTimeMillis() + JWT_TOKEN_EXPIRATION_TIME)).
+                signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsernameFromToken(token);
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token);
+    }
+
+}
