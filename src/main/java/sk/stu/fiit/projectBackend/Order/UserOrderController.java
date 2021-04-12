10bbd60ef44045eabaabeed0f0a67550e500cba2
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-12 20:02:41 +0200

    Added: Search also returns ID of tour offers

--- src/main/java/sk/stu/fiit/projectBackend/Order/UserOrderController.java
+++ src/main/java/sk/stu/fiit/projectBackend/Order/UserOrderController.java
@@ -5,8 +5,15 @@
  */
 package sk.stu.fiit.projectBackend.Order;
 
+import javax.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sk.stu.fiit.projectBackend.Order.dto.BookedToursResponse;
+import sk.stu.fiit.projectBackend.TourOffer.dto.DataPage;
 
 /**
  *
@@ -14,6 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("api/v1/users/orders")
+@AllArgsConstructor
 public class UserOrderController {
+    
+    private final UserOrderService userOrderService;
+    
+    @GetMapping(path = "/")
+    public ResponseEntity<Page<BookedToursResponse>> getBookedTours(@Valid DataPage page) {
+        return ResponseEntity.ok(userOrderService.getBookedTours(page));
+    }
     
 }
