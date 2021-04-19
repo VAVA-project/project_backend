7b3f0524bb0bbc15a7f509261323bfd4c8fa4edc
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-19 19:52:52 +0200

    Fixed: Issue with last element in search response

--- src/main/java/sk/stu/fiit/projectBackend/Search/SearchController.java
+++ src/main/java/sk/stu/fiit/projectBackend/Search/SearchController.java
@@ -5,18 +5,14 @@
  */
 package sk.stu.fiit.projectBackend.Search;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sk.stu.fiit.projectBackend.TourOffer.TourOffer;
 import sk.stu.fiit.projectBackend.TourOffer.dto.DataPage;
 import sk.stu.fiit.projectBackend.TourOffer.dto.TourOfferResponse;
 
@@ -36,13 +32,7 @@ public class SearchController {
             @RequestParam("q") String query,
             @Valid DataPage page
     ) {
-        Page<TourOffer> offers = searchService.searchTourOffers(query, page);
-        List<TourOfferResponse> mappedOffers = offers.getContent().stream().map(
-                TourOfferResponse::new).collect(Collectors.toList());
-        
-        Page<TourOfferResponse> response = new PageImpl<>(mappedOffers, offers.getPageable(), mappedOffers.size());
-        
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(searchService.searchTourOffers(query, page));
     }
     
 }
